a47911a4600ad15eaebaa82e837f3828d43bba83
hiyampatel <hiya.patel@innoraft.com>
2021-04-08 20:36:34 +0530

    Php code using OOPs - 1

--- form.php
+++ form.php
@@ -6,67 +6,77 @@
     </head>
     <body>
         <?php
-            $first = $last = "";
-            $count = 0;
-            $error1 = $error2 = "";
+            $error1 = [];
 
-            if($_SERVER["REQUEST_METHOD"] == "POST")
+            class user_validate
             {
-                if(empty($_POST["first"]))
+                private $name,$val;
+                public $error=[];
+
+
+                private static $field = ['first', 'last'];
+
+                public function __construct($post_data)
                 {
-                    $error1 = "Required";
-                }
-                else
-                {
-                    $first = validate_str($_POST['first']);
-                    if (!preg_match("/^[a-zA-Z- ]*$/", $first))
-                    {
-                        $error1 = "Characters Only";
-                    }
-                    else{
-                        $count += 1;
-                    }
+                    $this->name = $post_data;
                 }
 
-                if(empty($_POST["last"]))
+                public function validateForm()
                 {
-                    $error2 = "Required";
-                }
-                else
-                {
-                    $last = validate_str($_POST['last']);
-                    if (!preg_match("/^[a-zA-Z- ]*$/", $last))
+                    foreach(self::$field as $value)
                     {
-                        $error2 = "Characters Only";
+                        if(empty($this->name[$value]))
+                        {
+                            $this->$error[$value] = "Required";
+                        }
+                        else
+                        {
+                            $n = $this->validate_str($this->$name[$value]);
+
+                            if (!preg_match("/^[a-zA-Z- ]*$/", $value))
+                            {
+                                $this->$error[$value] = "Characters Only";
+                            }
+                            else{
+                                $this->$val = $this->$val.$value;
+                            }
+                        }
                     }
-                    else{
-                        $count += 1;
-                    }
+
+
                 }
 
-                if ($count == 2){
-                    $val = $first . " " . $last;
+
+                private function validate_str($string)
+                {
+                    $string = trim($string);
+                    $string = stripslashes($string);
+                    $string = htmlspecialchars($string);
+                    return $string;
                 }
+
+            }
+
+            if(isset($_POST['submit']))
+            {
+
+                $validation = new user_validate($_POST);
+                $error1 = $validation->validateForm();
             }
 
 
-            function validate_str($string){
-                $string = trim($string);
-                $string = stripslashes($string);
-                $string = htmlspecialchars($string);
-                return $string;
-            }
+
         ?>
 
 
         <h1>Basic Form</h1>
         <form method="POST" action="form.php">
-            First : <input type="text" name="first"><span><?php echo $error1;?></span><br><br>
-            Last : <input type="text" name="last"><span><?php echo $error2;?></span><br><br>
-            Full Name : <input type="type" id="full" name="full" value="<?php echo $first." ".$last?>" readonly><br><br>
+            First : <input type="text" name="first"><span><?php echo $error1['first'];?></span><br><br>
+            Last : <input type="text" name="last"><span><?php echo $error1['last'];?></span><br><br>
+            Full Name : <input type="type" id="full" name="full" value="" readonly><br><br>
             <input type="submit" name="submit" value="Submit">
         </form>
 
-        <h2><?php echo $val?></h2>
+        <h2></h2>
     <body>
 </html>
