d343f1228a2e897be3c81b81fc9e42ec0da3c8ab
hiyampatel <hiya.patel@innoraft.com>
2021-04-08 20:34:44 +0530

    Php code without OOPs

--- form.php
+++ form.php
@@ -5,15 +5,68 @@
         <title>Form</title>
     </head>
     <body>
-        
+        <?php
+            $first = $last = "";
+            $count = 0;
+            $error1 = $error2 = "";
+
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                if(empty($_POST["first"]))
+                {
+                    $error1 = "Required";
+                }
+                else
+                {
+                    $first = validate_str($_POST['first']);
+                    if (!preg_match("/^[a-zA-Z- ]*$/", $first))
+                    {
+                        $error1 = "Characters Only";
+                    }
+                    else{
+                        $count += 1;
+                    }
+                }
+
+                if(empty($_POST["last"]))
+                {
+                    $error2 = "Required";
+                }
+                else
+                {
+                    $last = validate_str($_POST['last']);
+                    if (!preg_match("/^[a-zA-Z- ]*$/", $last))
+                    {
+                        $error2 = "Characters Only";
+                    }
+                    else{
+                        $count += 1;
+                    }
+                }
+
+                if ($count == 2){
+                    $val = $first . " " . $last;
+                }
+            }
+
+
+            function validate_str($string){
+                $string = trim($string);
+                $string = stripslashes($string);
+                $string = htmlspecialchars($string);
+                return $string;
+            }
+        ?>
+
+
         <h1>Basic Form</h1>
         <form method="POST" action="form.php">
-            First : <input type="text" name="first"><span></span><br><br>
-            Last : <input type="text" name="last"><span></span><br><br>
-            Full Name : <input type="type" id="full" name="full" value="" readonly><br><br>
+            First : <input type="text" name="first"><span><?php echo $error1;?></span><br><br>
+            Last : <input type="text" name="last"><span><?php echo $error2;?></span><br><br>
+            Full Name : <input type="type" id="full" name="full" value="<?php echo $first." ".$last?>" readonly><br><br>
             <input type="submit" name="submit" value="Submit">
         </form>
 
-        <h2></h2>
+        <h2><?php echo $val?></h2>
     <body>
 </html>
