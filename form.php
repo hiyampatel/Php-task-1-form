adc66c054d1848607b28b72e34e4625b98274cfa
hiyampatel <hiya.patel@innoraft.com>
2021-04-08 18:04:54 +0530

    Basic form created

---
+++ form.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form</title>
+    </head>
+    <body>
+        
+        <h1>Basic Form</h1>
+        <form method="POST" action="form.php">
+            First : <input type="text" name="first"><span></span><br><br>
+            Last : <input type="text" name="last"><span></span><br><br>
+            Full Name : <input type="type" id="full" name="full" value="" readonly><br><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+        <h2></h2>
+    <body>
+</html>
