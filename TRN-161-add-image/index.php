729f47b671065defaaeb6db316bdfd0b5d25faf8
hiyampatel <hiya.patel@innoraft.com>
2021-04-09 12:44:33 +0530

    Basic form created

---
+++ TRN-161-add-image/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Image_Upload</title>
+</head>
+<body>
+
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="file_upload">
+        <input type="submit" name="submit" value="Upload an Image">
+    </form>
+
+</body>
+</html>
