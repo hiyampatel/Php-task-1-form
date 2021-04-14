8a2c4909aaaaba40c00f1d1a34eb00ecc47ad03b
hiyampatel <hiya.patel@innoraft.com>
2021-04-14 18:08:33 +0530

    TRN-180: Displayed data to front page

--- TRN-180-rest-api/display.php
+++ TRN-180-rest-api/display.php
@@ -14,7 +14,7 @@ class Display_Data
         return $this->data;
     }
 
-    private function get_data($url)
+    public function get_data($url)
     {
         $client = new GuzzleHttp\Client();
 
@@ -25,6 +25,30 @@ class Display_Data
         $v = json_decode($json, true);
 
         return $v;
+    }
+
+
+    public function get_img_url($url)
+    {
+        $img_add_1 = $this->get_data($url);
+        $img_1 = 'https://www.innoraft.com' . $img_add_1['data']['attributes']['uri']['url'];
+
+        return $img_1;
+    }
+
+    public function get_icon_urls($url)
+    {
+        $icon_list = [];
+        $i = $this->get_data($url);
+        foreach ($i['data'] as $key => $value) {
+
+            $icon_1 = $value['relationships']['field_media_image']['links']['related']['href'];
+            $icon = $this->get_img_url($icon_1);
+            $icon_list[$key] = $icon;
+
+        }
+
+        return $icon_list;
     }
 
 }
--- TRN-180-rest-api/index.php
+++ TRN-180-rest-api/index.php
@@ -6,6 +6,110 @@ require 'display.php';
 $display = new Display_Data();
 $output = $display->display();
 
-print_r($output);
+$img_1 = $display->get_img_url($output['data']['15']['relationships']['field_image']['links']['related']['href']);
+$img_2 = $display->get_img_url($output['data']['12']['relationships']['field_image']['links']['related']['href']);
+$img_3 = $display->get_img_url($output['data']['13']['relationships']['field_image']['links']['related']['href']);
+$img_4 = $display->get_img_url($output['data']['14']['relationships']['field_image']['links']['related']['href']);
+
+$icon_list_1 = $display->get_icon_urls($output['data']['15']['relationships']['field_service_icon']['links']['related']['href']);
+$icon_list_2 = $display->get_icon_urls($output['data']['12']['relationships']['field_service_icon']['links']['related']['href']);
+$icon_list_3 = $display->get_icon_urls($output['data']['13']['relationships']['field_service_icon']['links']['related']['href']);
+$icon_list_4 = $display->get_icon_urls($output['data']['15']['relationships']['field_service_icon']['links']['related']['href']);
+
+
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data</title>
+</head>
+<body>
+
+    <div class="section-1">
+        <div class="left">
+            <img src="<?php echo $img_1; ?>" alt="img-1" width="<?php echo $output['data']['15']['relationships']['field_image']['data']['meta']['width'];?>" height="<?php echo $output['data']['15']['relationships']['field_image']['data']['meta']['height'];?>">
+        </div>
+        <div class="right">
+            <h1><?php echo $output['data']['15']['attributes']['title'];?> </h1>
+
+            <?php
+                foreach ($icon_list_1 as $k => $v) {
+                    echo "<img src=" . $v . " height='60'>";
+                }
+            ?>
+
+            <ul>
+                <li><?php echo $output['data']['2']['attributes']['title'];?></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+    <div class="section-2">
+        <div class="right">
+            <h1><?php echo $output['data']['12']['attributes']['title'];?> </h1>
+            <?php
+                foreach ($icon_list_2 as $k => $v) {
+                    echo "<img src=" . $v . " height='60'>";
+                }
+            ?>
+            <ul>
+                <li><?php echo $output['data']['0']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['7']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['11']['attributes']['title'];?></li>
+            </ul>
+        </div>
+        <div class="left">
+            <img src="<?php echo $img_2; ?>" alt="img-1" width="<?php echo $output['data']['12']['relationships']['field_image']['data']['meta']['width'];?>" height="<?php echo $output['data']['12']['relationships']['field_image']['data']['meta']['height'];?>">
+        </div>
+    </div>
+
+
+
+    <div class="section-3">
+        <div class="left">
+            <img src="<?php echo $img_3; ?>" alt="img-1" width="<?php echo $output['data']['13']['relationships']['field_image']['data']['meta']['width'];?>" height="<?php echo $output['data']['13']['relationships']['field_image']['data']['meta']['height'];?>">
+        </div>
+        <div class="right">
+            <h1><?php echo $output['data']['13']['attributes']['title'];?> </h1>
+            <?php
+                foreach ($icon_list_3 as $k => $v) {
+                    echo "<img src=" . $v . " height='60'>";
+                }
+            ?>
+            <ul>
+                <li><?php echo $output['data']['1']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['3']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['8']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['9']['attributes']['title'];?></li>
+            </ul>
+        </div>
+    </div>
+
+
+
+    <div class="section-4">
+        <div class="right">
+            <h1><?php echo $output['data']['14']['attributes']['title'];?> </h1>
+            <?php
+                foreach ($icon_list_4 as $k => $v) {
+                    echo "<img src=" . $v . " height='60'>";
+                }
+            ?>
+            <ul>
+                <li><?php echo $output['data']['5']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['6']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['10']['attributes']['title'];?></li>
+                <li><?php echo $output['data']['4']['attributes']['title'];?></li>
+            </ul>
+        </div>
+        <div class="left">
+            <img src="<?php echo $img_4; ?>" alt="img-1" width="<?php echo $output['data']['14']['relationships']['field_image']['data']['meta']['width'];?>" height="<?php echo $output['data']['14']['relationships']['field_image']['data']['meta']['height'];?>">
+        </div>
+    </div>
+
+</body>
+</html>
