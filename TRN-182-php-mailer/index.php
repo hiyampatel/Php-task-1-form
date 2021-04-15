ffeabd63a8ea99f4b55fb4ccce8bf88f715950f1
hiyampatel <hiya.patel@innoraft.com>
2021-04-15 12:01:48 +0530

    TRN-182: Created html page and added the requirements

---
+++ TRN-182-php-mailer/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'send.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mailer</title>
+</head>
+<body>
+    <h1>PHP mailer</h1>
+    <form action="" method="POST">
+        Email: <input type="email" name="email"><br><br>
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
---
+++ TRN-182-php-mailer/vendor/composer/installed.php
@@ -0,0 +1,36 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1fa64da921b10d7501d5137db33f5c8d0a70d6e0',
+    'name' => '__root__',
+    'dev' => true,
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1fa64da921b10d7501d5137db33f5c8d0a70d6e0',
+      'dev-requirement' => false,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.4.0',
+      'version' => '6.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '050d430203105c27c30efd1dce7aa421ad882d01',
+      'dev-requirement' => false,
+    ),
+  ),
+);
